79b458d0df9b44113a82752b9727063af5375831
Doot-s <18lean2005@gmail.com>
2024-11-13 09:31:38 +0800

    Create LaboratoryExercise.java
    aaaaaaaaaaaaaa

---
+++ LaboratoryExercise.java
@@ -0,0 +1,18 @@
+package laboratoryexercise;
+
+public class Laboratoryexercise {
+
+    public static void main(String[] args) {
+
+        int iNumber = 3;
+
+        if (iNumber >= 0) {
+            if (iNumber == 0) {
+                System.out.println("First String");
+            } else {
+                System.out.println("Second String");
+                System.out.println("Third String");
+            }
+        }
+    }
+}
